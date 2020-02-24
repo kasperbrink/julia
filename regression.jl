8ecd6cb05a158126a65fa57109ff4bc354177d20
kasperbrink <kasperbrinkjensen@gmail.com>
2020-02-24 09:59:59 +0000

    more regression

--- regression.jl
+++ regression.jl
@@ -2,3 +2,11 @@ using DataFrames, GLM, RDatasets
 
 quine = dataset("MASS", "quine")
 nbrmodel = glm(@formula(Days ~ Eth+Sex+Age+Lrn), quine, NegativeBinomial(2.0), LogLink())
+
+form = dataset("datasets", "Formaldehyde")
+lm1 = fit(LinearModel, @formula(OptDen ~ Carb), form)
+
+form[:, :Carb]
+
+sum((predict(lm1)- form[:, :OptDen]).^2)
+
