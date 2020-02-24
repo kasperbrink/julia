2d4b0bf377a8d3e62e802ccf6ec6ab36cba688e5
kasperbrink <kasperbrinkjensen@gmail.com>
2020-02-24 08:16:56 +0000

    functioning loop

--- julia_test.jl
+++ julia_test.jl
@@ -1,18 +1,14 @@
 a = [3x + 2y + z for x in 1:2, y in 2:3, z in 1:2]
 search( needle::AbstractString, heystack::AbstractString )= something(findfirst(heystack,needle), 0:-1);
 
+
 letters = join('a':'z')
 name = "navn navnesen"
 
-split(name, "")
-findall(name[1], letters)
 
-findall("a", split(letters, ""))
-
-
-    for m in eachmatch(r"$name_pos", "My cats and my dog")
-           println("Matched $(m.match) at index $(m.offset)")
-    end
+for i in 1:length(name), m in eachmatch(Regex(string(name[i])), letters)
+    println("Matched $(m.match) at index $(m.offset)")
+    
 end
 
 
